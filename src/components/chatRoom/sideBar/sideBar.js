7f5147a2aa71690440a6506f6318d27bd7d12bf1
huysg136 <thaigiahuy6912@gmail.com>
2025-11-04 23:06:58 +0700

    fix

--- src/components/chatRoom/sideBar/sideBar.js
+++ src/components/chatRoom/sideBar/sideBar.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Row, Col } from 'antd';
-import RoomList from '../roomList/roomList';
 import Searching from '../searching/searching';
+import RoomList from '../roomList/roomList';
 import './sideBar.scss';
 
 export default function SideBar() {
   return (
     <div className="sidebar-wrapper">
-      <Row>
-        <Col span={24}><Searching /></Col>
-        <Col span={24}><RoomList /></Col>
-      </Row>
+      <div className="search-wrapper">
+        <Searching />
+      </div>
+      <div className="rooms-wrapper">
+        <RoomList />
+      </div>
     </div>
   );
 }
