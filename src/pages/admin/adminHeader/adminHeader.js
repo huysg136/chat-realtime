f5c6e6361021bfc04d4eb817f3ee3aec44b5394e
huysg136 <thaigiahuy6912@gmail.com>
2026-02-01 14:39:56 +0700

    feat: implement AdminHeader component displaying site title and authenticated user details.

--- src/pages/admin/adminHeader/adminHeader.js
+++ src/pages/admin/adminHeader/adminHeader.js
@@ -2,27 +2,19 @@ import React, { useContext } from "react";
 import { Layout, Avatar, Space } from "antd";
 import { AuthContext } from "../../../context/authProvider";
 import "./adminHeader.scss";
-import { useUserData } from "../../../hooks/useUserData";
 
 const { Header } = Layout;
 
 export default function AdminHeader() {
   const { user } = useContext(AuthContext);
-  const { role, photoURL, displayName, loading } = useUserData(user.uid);
+
   return (
     <Header className="admin-header">
       <h2>Trang quản trị quik.id.vn</h2>
-      { loading ? (
-        null
-      ) : (
-        <Space className="admin-user">
-          <Avatar src={photoURL} />
-          <div className="admin-profile">
-            <span>{displayName}</span>
-            <span>{role}</span>
-          </div>
-        </Space>
-      )} 
+      <Space className="admin-user">
+        <Avatar src={user?.photoURL} />
+        <span>{user?.displayName || "Admin"}</span>
+      </Space>
     </Header>
   );
 }
